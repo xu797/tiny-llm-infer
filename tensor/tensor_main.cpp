083e3427d1ad75c3c39b9f998dfb23c4d2c07a34
xujixian <xjxian02@gmail.com>
2026-09-18 16:07:50 +0800

    format files...

--- tensor/tensor_main.cpp
+++ tensor/tensor_main.cpp
@@ -1,13 +1,14 @@
 #include <glog/logging.h>
 #include <gtest/gtest.h>
-
 #include <iostream>
-int main(int argc, char *argv[]) {
-  testing::InitGoogleTest(&argc, argv);
-  google::InitGoogleLogging("my_vllm");
-  FLAGS_log_dir = "../log";
-  FLAGS_alsologtostderr = true;
 
-  LOG(INFO) << "Start test...\n";
-  return RUN_ALL_TESTS();
+int main(int argc, char *argv[]) 
+{
+    testing::InitGoogleTest(&argc, argv);
+    google::InitGoogleLogging("my_vllm");
+    FLAGS_log_dir = "../log";
+    FLAGS_alsologtostderr = true;
+
+    LOG(INFO) << "Start test...\n";
+    return RUN_ALL_TESTS();
 }
--- tensor/test/test_tensor.cpp
+++ tensor/test/test_tensor.cpp
@@ -2,7 +2,6 @@
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 
-
 #include "tensor.h"
 #include "utils.cuh"
 #include "buffer.h"
@@ -10,141 +9,158 @@
 #include "cpu_alloc.h"
 #include "cuda_alloc.h"
 
-TEST(test_tensor, to_cpu) 
+TEST(test_tensor, to_cpu)
 {
-  using namespace my_vllm;
-  auto alloc_cu = CUDADeviceAllocatorFactory::get_instance();
-  Tensor t1_cu(DataType::kDataTypeFp32, 32, 32, true, alloc_cu);
-  ASSERT_EQ(t1_cu.is_empty(), false);
-  set_value_cu(t1_cu.ptr<float>(), 32 * 32);
+    using namespace my_vllm;
+    auto alloc_cu = CUDADeviceAllocatorFactory::get_instance();
+    Tensor t1_cu(DataType::kDataTypeFp32, 32, 32, true, alloc_cu);
+    ASSERT_EQ(t1_cu.is_empty(), false);
+    set_value_cu(t1_cu.ptr<float>(), 32 * 32);
 
-  t1_cu.to_cpu();
-  ASSERT_EQ(t1_cu.device_type(), DeviceType::kDeviceCPU);
-  float* cpu_ptr = t1_cu.ptr<float>();
-  for (int i = 0; i < 32 * 32; ++i) {
-    ASSERT_EQ(*(cpu_ptr + i), 1.f);
-  }
+    t1_cu.to_cpu();
+    ASSERT_EQ(t1_cu.device_type(), DeviceType::kDeviceCPU);
+    float *cpu_ptr = t1_cu.ptr<float>();
+    for (int i = 0; i < 32 * 32; ++i)
+    {
+        ASSERT_EQ(*(cpu_ptr + i), 1.f);
+    }
 }
 
-TEST(test_tensor, clone_cuda) {
-  using namespace my_vllm;
-  auto alloc_cu = CUDADeviceAllocatorFactory::get_instance();
-  Tensor t1_cu(DataType::kDataTypeFp32, 32, 32, true, alloc_cu);
-  ASSERT_EQ(t1_cu.is_empty(), false);
-  set_value_cu(t1_cu.ptr<float>(), 32 * 32, 1.f);
+TEST(test_tensor, clone_cuda)
+{
+    using namespace my_vllm;
+    auto alloc_cu = CUDADeviceAllocatorFactory::get_instance();
+    Tensor t1_cu(DataType::kDataTypeFp32, 32, 32, true, alloc_cu);
+    ASSERT_EQ(t1_cu.is_empty(), false);
+    set_value_cu(t1_cu.ptr<float>(), 32 * 32, 1.f);
 
-  Tensor t2_cu = t1_cu.clone();
-  float* p2 = new float[32 * 32];
-  cudaMemcpy(p2, t2_cu.ptr<float>(), sizeof(float) * 32 * 32, cudaMemcpyDeviceToHost);
-  for (int i = 0; i < 32 * 32; ++i) {
-    ASSERT_EQ(p2[i], 1.f);
-  }
+    Tensor t2_cu = t1_cu.clone();
+    float *p2 = new float[32 * 32];
+    cudaMemcpy(p2, t2_cu.ptr<float>(), sizeof(float) * 32 * 32, cudaMemcpyDeviceToHost);
+    for (int i = 0; i < 32 * 32; ++i)
+    {
+        ASSERT_EQ(p2[i], 1.f);
+    }
 
-  cudaMemcpy(p2, t1_cu.ptr<float>(), sizeof(float) * 32 * 32, cudaMemcpyDeviceToHost);
-  for (int i = 0; i < 32 * 32; ++i) {
-    ASSERT_EQ(p2[i], 1.f);
-  }
+    cudaMemcpy(p2, t1_cu.ptr<float>(), sizeof(float) * 32 * 32, cudaMemcpyDeviceToHost);
+    for (int i = 0; i < 32 * 32; ++i)
+    {
+        ASSERT_EQ(p2[i], 1.f);
+    }
 
-  ASSERT_EQ(t2_cu.data_type(), DataType::kDataTypeFp32);
-  ASSERT_EQ(t2_cu.size(), 32 * 32);
+    ASSERT_EQ(t2_cu.data_type(), DataType::kDataTypeFp32);
+    ASSERT_EQ(t2_cu.size(), 32 * 32);
 
-  t2_cu.to_cpu();
-  std::memcpy(p2, t2_cu.ptr<float>(), sizeof(float) * 32 * 32);
-  for (int i = 0; i < 32 * 32; ++i) {
-    ASSERT_EQ(p2[i], 1.f);
-  }
-  delete[] p2;
+    t2_cu.to_cpu();
+    std::memcpy(p2, t2_cu.ptr<float>(), sizeof(float) * 32 * 32);
+    for (int i = 0; i < 32 * 32; ++i)
+    {
+        ASSERT_EQ(p2[i], 1.f);
+    }
+    delete[] p2;
 }
 
-TEST(test_tensor, clone_cpu) {
-  using namespace my_vllm;
-  auto alloc_cpu = CPUDeviceAllocatorFactory::get_instance();
-  Tensor t1_cpu(DataType::kDataTypeFp32, 32, 32, true, alloc_cpu);
-  ASSERT_EQ(t1_cpu.is_empty(), false);
-  for (int i = 0; i < 32 * 32; ++i) {
-    t1_cpu.index<float>(i) = 1.f;
-  }
+TEST(test_tensor, clone_cpu)
+{
+    using namespace my_vllm;
+    auto alloc_cpu = CPUDeviceAllocatorFactory::get_instance();
+    Tensor t1_cpu(DataType::kDataTypeFp32, 32, 32, true, alloc_cpu);
+    ASSERT_EQ(t1_cpu.is_empty(), false);
+    for (int i = 0; i < 32 * 32; ++i)
+    {
+        t1_cpu.index<float>(i) = 1.f;
+    }
 
-  Tensor t2_cpu = t1_cpu.clone();
-  float* p2 = new float[32 * 32];
-  std::memcpy(p2, t2_cpu.ptr<float>(), sizeof(float) * 32 * 32);
-  for (int i = 0; i < 32 * 32; ++i) {
-    ASSERT_EQ(p2[i], 1.f);
-  }
+    Tensor t2_cpu = t1_cpu.clone();
+    float *p2 = new float[32 * 32];
+    std::memcpy(p2, t2_cpu.ptr<float>(), sizeof(float) * 32 * 32);
+    for (int i = 0; i < 32 * 32; ++i)
+    {
+        ASSERT_EQ(p2[i], 1.f);
+    }
 
-  std::memcpy(p2, t1_cpu.ptr<float>(), sizeof(float) * 32 * 32);
-  for (int i = 0; i < 32 * 32; ++i) {
-    ASSERT_EQ(p2[i], 1.f);
-  }
-  delete[] p2;
+    std::memcpy(p2, t1_cpu.ptr<float>(), sizeof(float) * 32 * 32);
+    for (int i = 0; i < 32 * 32; ++i)
+    {
+        ASSERT_EQ(p2[i], 1.f);
+    }
+    delete[] p2;
 }
 
-TEST(test_tensor, to_cu) {
-  using namespace my_vllm;
-  auto alloc_cpu = CPUDeviceAllocatorFactory::get_instance();
-  Tensor t1_cpu(DataType::kDataTypeFp32, 32, 32, true, alloc_cpu);
-  ASSERT_EQ(t1_cpu.is_empty(), false);
-  float* p1 = t1_cpu.ptr<float>();
-  for (int i = 0; i < 32 * 32; ++i) {
-    *(p1 + i) = 1.f;
-  }
+TEST(test_tensor, to_cu)
+{
+    using namespace my_vllm;
+    auto alloc_cpu = CPUDeviceAllocatorFactory::get_instance();
+    Tensor t1_cpu(DataType::kDataTypeFp32, 32, 32, true, alloc_cpu);
+    ASSERT_EQ(t1_cpu.is_empty(), false);
+    float *p1 = t1_cpu.ptr<float>();
+    for (int i = 0; i < 32 * 32; ++i)
+    {
+        *(p1 + i) = 1.f;
+    }
 
-  t1_cpu.to_cuda();
-  float* p2 = new float[32 * 32];
-  cudaMemcpy(p2, t1_cpu.ptr<float>(), sizeof(float) * 32 * 32, cudaMemcpyDeviceToHost);
-  for (int i = 0; i < 32 * 32; ++i) {
-    ASSERT_EQ(*(p2 + i), 1.f);
-  }
-  delete[] p2;
+    t1_cpu.to_cuda();
+    float *p2 = new float[32 * 32];
+    cudaMemcpy(p2, t1_cpu.ptr<float>(), sizeof(float) * 32 * 32, cudaMemcpyDeviceToHost);
+    for (int i = 0; i < 32 * 32; ++i)
+    {
+        ASSERT_EQ(*(p2 + i), 1.f);
+    }
+    delete[] p2;
 }
 
-TEST(test_tensor, init1) {
-  using namespace my_vllm;
-  auto alloc_cu = CPUDeviceAllocatorFactory::get_instance();
+TEST(test_tensor, init1)
+{
+    using namespace my_vllm;
+    auto alloc_cu = CPUDeviceAllocatorFactory::get_instance();
 
-  int32_t size = 32 * 151;
+    int32_t size = 32 * 151;
 
-  Tensor t1(DataType::kDataTypeFp32, size, true, alloc_cu);
-  ASSERT_EQ(t1.is_empty(), false);
+    Tensor t1(DataType::kDataTypeFp32, size, true, alloc_cu);
+    ASSERT_EQ(t1.is_empty(), false);
 }
 
-TEST(test_tensor, init3) {
-  using namespace my_vllm;
-  float* ptr = new float[32];
-  ptr[0] = 31;
-  Tensor t1(DataType::kDataTypeFp32, 32, false, nullptr, ptr);
-  ASSERT_EQ(t1.is_empty(), false);
-  ASSERT_EQ(t1.ptr<float>(), ptr);
-  ASSERT_EQ(*t1.ptr<float>(), 31);
+TEST(test_tensor, init3)
+{
+    using namespace my_vllm;
+    float *ptr = new float[32];
+    ptr[0] = 31;
+    Tensor t1(DataType::kDataTypeFp32, 32, false, nullptr, ptr);
+    ASSERT_EQ(t1.is_empty(), false);
+    ASSERT_EQ(t1.ptr<float>(), ptr);
+    ASSERT_EQ(*t1.ptr<float>(), 31);
 }
 
-TEST(test_tensor, init2) {
-  using namespace my_vllm;
-  auto alloc_cu = CPUDeviceAllocatorFactory::get_instance();
+TEST(test_tensor, init2)
+{
+    using namespace my_vllm;
+    auto alloc_cu = CPUDeviceAllocatorFactory::get_instance();
 
-  int32_t size = 32 * 151;
+    int32_t size = 32 * 151;
 
-  Tensor t1(DataType::kDataTypeFp32, size, false, alloc_cu);
-  ASSERT_EQ(t1.is_empty(), true);
+    Tensor t1(DataType::kDataTypeFp32, size, false, alloc_cu);
+    ASSERT_EQ(t1.is_empty(), true);
 }
 
-TEST(test_tensor, assign1) {
-  using namespace my_vllm;
-  auto alloc_cpu = CPUDeviceAllocatorFactory::get_instance();
-  Tensor t1_cpu(DataType::kDataTypeFp32, 32, 32, true, alloc_cpu);
-  ASSERT_EQ(t1_cpu.is_empty(), false);
+TEST(test_tensor, assign1)
+{
+    using namespace my_vllm;
+    auto alloc_cpu = CPUDeviceAllocatorFactory::get_instance();
+    Tensor t1_cpu(DataType::kDataTypeFp32, 32, 32, true, alloc_cpu);
+    ASSERT_EQ(t1_cpu.is_empty(), false);
 
-  int32_t size = 32 * 32;
-  float* ptr = new float[size];
-  for (int i = 0; i < size; ++i) {
-    ptr[i] = float(i);
-  }
-  std::shared_ptr<Buffer> buffer =
-      std::make_shared<Buffer>(size * sizeof(float), nullptr, ptr, true);
-  buffer->set_device_type(DeviceType::kDeviceCPU);
+    int32_t size = 32 * 32;
+    float *ptr = new float[size];
+    for (int i = 0; i < size; ++i)
+    {
+        ptr[i] = float(i);
+    }
+    std::shared_ptr<Buffer> buffer =
+            std::make_shared<Buffer>(size * sizeof(float), nullptr, ptr, true);
+    buffer->set_device_type(DeviceType::kDeviceCPU);
 
-  ASSERT_EQ(t1_cpu.assign(buffer), true);
-  ASSERT_EQ(t1_cpu.is_empty(), false);
-  ASSERT_NE(t1_cpu.ptr<float>(), nullptr);
-  delete[] ptr;
+    ASSERT_EQ(t1_cpu.assign(buffer), true);
+    ASSERT_EQ(t1_cpu.is_empty(), false);
+    ASSERT_NE(t1_cpu.ptr<float>(), nullptr);
+    delete[] ptr;
 }
